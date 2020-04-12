2ae1a4352cbd162aef17c7eeb351d2f6b19fd385
kovee <kovee@t-online.hu>
2020-04-12 15:16:31 +0200

    Code reorganization

---
+++ suisei/seed/core/constants.py
@@ -0,0 +1,27 @@
+## ============================================================================
+##                   **** SEED Virtual Reality Platform ****
+##                Copyright (C) 2019-2020, Suisei Entertainment
+## ============================================================================
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+## ============================================================================
+
+"""
+Contains global constants used by multiple SEED components.
+"""
+
+# The download link to update the GeoIP database.
+GEOIP_DOWNLOAD_LINK = \
+    'https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=pELDCVUneMIsHhyU&suffix=tar.gz'
